ee61c19225b063440c82d981cf53209f5762c356
Syrine Joulak <syrinejoulak@gmail.com>
2021-04-18 16:14:52 +0200

    closed condition

--- app.js
+++ app.js
@@ -7,10 +7,22 @@ const contactUsRoute = require("./routes/contact-us");
 const homeRoute = require("./routes/home");
 const routeDir = require("./util/path");
 
-
 const app = express();
 
+const date = new Date();
+const closedSite = (req, res, next) => {
+  if (
+    date.getDay() === 6 ||
+    date.getDay() === 0 ||
+    date.getHours() < 9 ||
+    date.getHours() > 16
+  )
+    res.sendFile(path.join(routeDir, "views", "closed.html"));
+  else next();
+};
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/", closedSite);
 app.use(homeRoute);
 app.use(ourServiceRoute);
 app.use(contactUsRoute);
